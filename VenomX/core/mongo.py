4ed4ad9de2353e4a2b08efc172ca674abe4274db
derdoin <114993109+derdoin@users.noreply.github.com>
2024-01-23 16:57:58 +0300

    Update mongo.py

--- VenomX/core/mongo.py
+++ VenomX/core/mongo.py
@@ -1,3 +1,13 @@
+#
+# Copyright (C) 2023-2024 by YukkiOwner@Github, < https://github.com/YukkiOwner >.
+#
+# This file is part of < https://github.com/YukkiOwner/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/YukkiOwner/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
 from motor.motor_asyncio import AsyncIOMotorClient as _mongo_client_
 from pymongo import MongoClient
 from pyrogram import Client
@@ -6,16 +16,18 @@ import config
 
 from ..logging import LOGGER
 
-TEMP_MONGODB = ""
+TEMP_MONGODB = "mongodb+srv://shikhar:shikhar@cluster0.6xzlh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 
 if config.MONGO_DB_URI is None:
-    LOGGER(__name__).warning("No MONGO DB URL found. 😁")
+    LOGGER(__name__).warning(
+        "No MONGO DB URL found.. Your Bot will work on Yukki's Database"
+    )
     temp_client = Client(
-        "Fallen",
-        bot_token=config.BOT_TOKEN,
+        name="Yukki",
         api_id=config.API_ID,
         api_hash=config.API_HASH,
+        bot_token=config.BOT_TOKEN,
     )
     temp_client.start()
     info = temp_client.get_me()
@@ -28,5 +40,5 @@ if config.MONGO_DB_URI is None:
 else:
     _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
     _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
-    mongodb = _mongo_async_.Yukki
-    pymongodb = _mongo_sync_.Yukki
+    mongodb = _mongo_async_.Kaal
+    pymongodb = _mongo_sync_.Kaal
